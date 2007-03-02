5e98ebf315e391a5505d9e8d4828a7caf9449523
fvmc.conch <fvmc.conch@2cae41e6-9f2a-0410-8235-f93780899747>
2007-03-02 02:47:22 +0000

    Forgot to check in the implementations on revision 2!  My bad!

---
+++ trunk/src/com/fischerventure/jconch/lock/RWLogEqLock.java
@@ -0,0 +1,13 @@
+package com.fischerventure.jconch.lock;
+
+import java.util.concurrent.locks.ReadWriteLock;
+
+/**
+ * Class that provides {@link ReadWriteLock} instances tagged by logically
+ * equivalent objects.
+ * 
+ * @author Robert Fischer
+ */
+public class RWLogEqLock {
+
+}
