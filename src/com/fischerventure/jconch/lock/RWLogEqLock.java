1bcc88410008ad7b77add7586420bb4d0d39c2c6
fvmc.conch <fvmc.conch@2cae41e6-9f2a-0410-8235-f93780899747>
2007-03-02 03:48:34 +0000

    RWLogEqLock now works just fine.

--- src/com/fischerventure/jconch/lock/RWLogEqLock.java
+++ src/com/fischerventure/jconch/lock/RWLogEqLock.java
@@ -1,6 +1,7 @@
 package com.fischerventure.jconch.lock;
 
 import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Class that provides {@link ReadWriteLock} instances tagged by logically
@@ -8,6 +9,31 @@ import java.util.concurrent.locks.ReadWriteLock;
  * 
  * @author Robert Fischer
  */
-public class RWLogEqLock {
+public class RWLogEqLock extends AbstractLogEqLock<ReadWriteLock> {
 
+	/**
+	 * The global instance.
+	 */
+	private static final RWLogEqLock global = new RWLogEqLock();
+
+	/**
+	 * Provides the same instance of this class every time.
+	 * 
+	 * @return A singleton instance.
+	 */
+	public static RWLogEqLock getGlobalInstance() {
+		return global;
+	}
+
+	/**
+	 * Constructor.
+	 */
+	public RWLogEqLock() {
+		// Does nothing.
+	}
+
+	@Override
+	protected ReadWriteLock createNewLock() {
+		return new ReentrantReadWriteLock();
+	}
 }
