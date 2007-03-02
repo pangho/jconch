fcd2396529700bef270d0c562e8d2f7210333bfd
fvmc.conch <fvmc.conch@2cae41e6-9f2a-0410-8235-f93780899747>
2007-03-02 03:45:02 +0000

    SyncLogEqLock works right now.

--- trunk/src/com/fischerventure/jconch/lock/AbstractLogEqLock.java
+++ trunk/src/com/fischerventure/jconch/lock/AbstractLogEqLock.java
@@ -19,6 +19,12 @@ abstract class AbstractLogEqLock<LOCK_T> {
 	private final LOCK_T nullLock = createNewLock();
 
 	/**
+	 * Holds onto references of objects, so that we don't lose the keys we're
+	 * using in the other map.
+	 */
+	private final Map<Object, Object> holder = new WeakHashMap<Object, Object>();
+
+	/**
 	 * The object providing locks.
 	 */
 	private final Map<Object, LOCK_T> locks = new WeakHashMap<Object, LOCK_T>();
@@ -37,16 +43,30 @@ abstract class AbstractLogEqLock<LOCK_T> {
 			return nullLock;
 		}
 
+		// Get the object to use as the key. We have to track this object
+		// because we want to guaranty consistancy.
+		final Object key;
+		final Object exists = holder.remove(in);
+		if (exists != null) {
+			// We had a previous version that was logically equivalent. We want
+			// to use that old one as the key for consistancy's sake.
+			key = exists;
+		} else {
+			// There was no previous version, so we're going to use this one as
+			// a new key.
+			key = in;
+		}
+		holder.put(in, key);
+
 		// The WeakHashMap only clears on put. If it clears on get, this falls
 		// apart. We can't use "remove" here, despite the how much we might like
-		// to, because that will clear the map.
-		final LOCK_T oldLock = locks.get(in);
+		// to, because that will clear the map. At least, I *THINK* that's true.
+		final LOCK_T oldLock = locks.remove(key);
 		final LOCK_T outLock;
 		if (oldLock != null) {
 			// If we overwrote a lock we were using before, put the old one
 			// back and return it. We need to wipe out the previous object
 			// reference -- otherwise, it will be GC'ed and we'll lose the lock.
-			locks.remove(in);
 			outLock = oldLock;
 		} else {
 			// There was no old lock, so we're using the newly-generated lock.
@@ -55,7 +75,7 @@ abstract class AbstractLogEqLock<LOCK_T> {
 		}
 
 		// Store and then return the lock of choice.
-		locks.put(in, outLock);
+		locks.put(key, outLock);
 		return outLock;
 	}
 
@@ -68,7 +88,7 @@ abstract class AbstractLogEqLock<LOCK_T> {
 	 *         provide a new lock.
 	 */
 	protected synchronized boolean hasLockFor(final Object obj) {
-		return this.locks.containsKey(obj);
+		return this.holder.containsKey(obj);
 	}
 
 	/**
