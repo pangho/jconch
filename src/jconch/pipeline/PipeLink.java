27268772eb040ef262b8bfeea6c0b176102f2118
fvmc.conch <fvmc.conch@2cae41e6-9f2a-0410-8235-f93780899747>
2007-05-02 23:47:53 +0000

    Started work on the pipeline.

---
+++ src/jconch/pipeline/PipeLink.java
@@ -0,0 +1,18 @@
+package jconch.pipeline;
+
+/**
+ * A link in the stages of the pipeline.
+ * 
+ * @author Robert Fischer
+ */
+public class PipeLink {
+
+    /**
+     * Determines if the pipe link has any elements currently in it.
+     * 
+     * @return If this link is empty.
+     */
+    public boolean isEmpty() {
+        return true;
+    }
+}
