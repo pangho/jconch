9cc95e74ed054218afeb07be28ad6f772d63a8b5
fvmc.conch <fvmc.conch@2cae41e6-9f2a-0410-8235-f93780899747>
2007-05-10 13:42:31 +0000

    Took out the R&B copyright.

--- src/jconch/pipeline/UnboundedPipeLink.java
+++ src/jconch/pipeline/UnboundedPipeLink.java
@@ -1,25 +1,3 @@
-/*
- * Project: JConch
- * @(#)$Id:  UnboundedPipeLink.java May 10, 2007 8:07:30 AM Robert Fischer $
- * 
- * This Software is confidential and copyrighted.
- * Copyright  2003, 2007 Room & Board, Inc.  All Rights Reserved.
- * 
- * This software is the proprietary information of Room & Board, Inc.  Use 
- * is subject to license terms.
- * 
- * Title to Software and all associated intellectual property rights is retained 
- * by Room & Board, Inc. and/or its licensors.
- * 
- * Except as specifically authorized in any Supplemental License Terms, this 
- * software may not be copied.  Unless enforcement is prohibited by 
- * applicable law, you may not modify, decompile, reverse engineer this 
- * Software without the express written permission of Room & Board, Inc.
- * 
- * No right, title or interest in or to any trademark, service mark, logo or 
- * trade name of Room & Board, Inc. or its licensors is granted under 
- * this Agreement.
- */
 package jconch.pipeline;
 
 import java.util.Queue;
