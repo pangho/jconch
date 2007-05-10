193304e7b6ac2977167a72b6fadb5e96a654d78b
fvmc.conch <fvmc.conch@2cae41e6-9f2a-0410-8235-f93780899747>
2007-05-10 19:48:10 +0000

    Changed to requiring a BlockingQueue of some flavor.

--- trunk/src/jconch/pipeline/UnboundedPipeLink.java
+++ trunk/src/jconch/pipeline/UnboundedPipeLink.java
@@ -1,6 +1,7 @@
 package jconch.pipeline;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.commons.lang.mutable.MutableLong;
@@ -30,86 +31,9 @@ import org.apache.commons.lang.mutable.MutableLong;
 public class UnboundedPipeLink<T> extends PipeLink<T> {
 
     /**
-     * The timeout on fetches.
-     */
-    private AtomicLong fetchTimeout = new AtomicLong(0L);
-
-    /**
      * Creates a new intance of <code>UnboundedPipeLink</code>.
-     * 
-     * @param queue
      */
-    protected UnboundedPipeLink() {
-        super(new ConcurrentLinkedQueue<T>());
-    }
-
-    /**
-     * Gets the timeout on fetch operations of the instance.
-     * 
-     * @return the fetch timeout.
-     */
-    public long getFetchTimeout() {
-        return fetchTimeout.get();
-    }
-
-    /**
-     * Sets the fetchTimeout of the instance. Positive values represent
-     * milliseconds to wait, and 0 means to not wait at all.
-     * 
-     * @param fetchTimeout
-     *            the fetch timeout to set
-     * @throws IllegalArgumentException
-     *             If the argument is < 0.
-     */
-    public void setFetchTimeout(final long newFetchTimeout) {
-        if (newFetchTimeout < 0) {
-            throw new IllegalArgumentException("Fetch timeout must be nonnegative");
-        }
-        fetchTimeout.set(newFetchTimeout);
-    }
-
-    /**
-     * In addition to the underlying behavior, this method acts on the fetch
-     * timeout setting (see the class documentation).
-     * 
-     * {@inheritDoc}
-     */
-    @Override
-    public T get() {
-        // TODO This logic should get promoted
-
-        // See if we get lucky and can bypass all the timeout logic
-        final T attempt = super.get();
-
-        // Did we get something?
-        if (attempt == null) {
-
-            // Drat. Okay, best to wait on the timeout
-            final long startTime = System.currentTimeMillis();
-            final long fullTimeout = getFetchTimeout();
-            for (long timeout = fullTimeout; timeout > 0; timeout = fullTimeout
-                    - (System.currentTimeMillis() - startTime)) {
-                try {
-                    boundary.wait(timeout);
-                } catch (InterruptedException ie) {
-                    // Do nothing
-                }
-
-                // Try to get something again
-                final T attempt2 = super.get();
-                if (attempt2 != null) {
-                    return attempt2;
-                }
-            }
-
-            // Didn't get anything in the timeout
-            return null;
-
-        } else {
-
-            // GOt something right off the bat. Sweet!
-            return attempt;
-
-        }
+    public UnboundedPipeLink() {
+        super(new LinkedBlockingQueue<T>());
     }
 }
