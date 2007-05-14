293bc04002894240a6b0809fc138b93230fb1d9a
fvmc.conch <fvmc.conch@2cae41e6-9f2a-0410-8235-f93780899747>
2007-05-14 12:49:37 +0000

    Updated ThreadingModels to use #isFinished instead of #isExhausted, and updated PipeLink to track weak references to their sources and sinks.

--- trunk/src/jconch/pipeline/PipeLink.java
+++ trunk/src/jconch/pipeline/PipeLink.java
@@ -2,98 +2,151 @@ package jconch.pipeline;
 
 import static java.lang.Math.max;
 
+import java.util.Set;
+import java.util.WeakHashMap;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.apache.commons.collections.set.MapBackedSet;
+import org.apache.commons.collections.set.SynchronizedSet;
 import org.apache.commons.lang.NullArgumentException;
 
 /**
+ * <p>
  * A link in the stages of the pipeline. This class takes responsibility for all
- * of the concurrency handling during hand-offs between pipelines.
+ * of the concurrency handling during hand-offs between pipelines. Furthermore,
+ * it abstracts the nature of the preceeding and succeeding elements -- as long
+ * as something feeds into this class or reads out of this class, it can be
+ * merged into a standard pipeline.
+ * </p>
+ * <p>
+ * The link requires at least one <em>source</em> and one <em>sink</em> to
+ * be registered before {@link #add(Object)} and {@link #get()} are usable. A
+ * source will place elements into the link, and a sink will draw elements from
+ * it. These registries are used for tracking status.
+ * </p>
  * 
  * @author Robert Fischer
  */
 public class PipeLink<T> {
 
-	private final BlockingQueue<T> q;
+    private final BlockingQueue<T> q;
 
-	/**
-	 * The timeout on fetches.
-	 */
-	private AtomicLong fetchTimeout = new AtomicLong(0L);
+    /**
+     * The timeout on fetches.
+     */
+    private AtomicLong fetchTimeout = new AtomicLong(0L);
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param queue
-	 *            The queue to use.
-	 * @throws NullArgumentException
-	 *             If the argument is <code>null</code>
-	 */
-	protected PipeLink(final BlockingQueue<T> queue) {
-		if (queue == null) {
-			throw new NullArgumentException("queue");
-		}
-		this.q = queue;
-	}
+    /**
+     * The sources of this link.
+     */
+    private final Set<Object> sources = SynchronizedSet.decorate(MapBackedSet.decorate(new WeakHashMap(2)));
 
-	/**
-	 * Adds an element into the link, if at all possible.
-	 * 
-	 * @param in
-	 *            The element to add.
-	 * @returns <code>true</code> if the add succeeded
-	 * @throws NullArgumentException
-	 *             If the argument is <code>null</code>
-	 */
-	public boolean add(final T in) {
-		if (in == null) {
-			throw new NullArgumentException("in");
-		}
-		final boolean out = q.offer(in);
-		return out;
-	}
+    /**
+     * The sinks of this link.
+     */
+    private final Set<Object> sinks = SynchronizedSet.decorate(MapBackedSet.decorate(new WeakHashMap(2)));
 
-	/**
-	 * Gets the timeout on fetch operations of the instance.
-	 * 
-	 * @return the fetch timeout.
-	 */
-	public long getFetchTimeout() {
-		return fetchTimeout.get();
-	}
+    /**
+     * Constructor.
+     * 
+     * @param queue
+     *            The queue to use.
+     * @throws NullArgumentException
+     *             If the argument is <code>null</code>
+     */
+    protected PipeLink(final BlockingQueue<T> queue) {
+        if (queue == null) {
+            throw new NullArgumentException("queue");
+        }
+        this.q = queue;
+    }
 
-	/**
-	 * Sets the fetch timeout of the instance. Positive values represent
-	 * milliseconds to wait, and 0 means to not wait at all.
-	 * 
-	 * @param fetchTimeout
-	 *            the fetch timeout to set
-	 * @throws IllegalArgumentException
-	 *             If the argument is < 0.
-	 */
-	public void setFetchTimeout(final long newFetchTimeout) {
-		if (newFetchTimeout < 0) {
-			throw new IllegalArgumentException(
-					"Fetch timeout must be nonnegative");
-		}
-		fetchTimeout.set(newFetchTimeout);
-	}
+    /**
+     * Registers a source for this pipe link.
+     * 
+     * @param source
+     * @throws NullArgumentException
+     *             If the argument is <code>null</code>
+     */
+    public void registerSource(final Object source) {
+        if (source == null) {
+            throw new NullArgumentException("source");
+        }
+        this.sources.add(source);
+    }
 
-	/**
-	 * Removes an element from the link, if any is available. This method may
-	 * block for up to {@link #getFetchTimeout()} milliseconds for a value to
-	 * become available.
-	 * 
-	 * @return The removed element, or <code>null</code> if the link is empty.
-	 */
-	public T get() {
-		try {
-			return q.poll(max(1, getFetchTimeout()), TimeUnit.MILLISECONDS);
-		} catch (InterruptedException ie) {
-			return null;
-		}
-	}
+    /**
+     * Registers a sink for this pipe link.
+     * 
+     * @param sink
+     * @throws NullArgumentException
+     *             If the argument is <code>null</code>
+     */
+    public void registerSink(final Object sink) {
+        if (sink == null) {
+            throw new NullArgumentException("sink");
+        }
+        this.sinks.add(sink);
+    }
+
+    /**
+     * Adds an element into the link, if at all possible.
+     * 
+     * @param in
+     *            The element to add.
+     * @returns <code>true</code> if the add succeeded
+     * @throws NullArgumentException
+     *             If the argument is <code>null</code>
+     */
+    public boolean add(final T in) {
+        if (in == null) {
+            throw new NullArgumentException("in");
+        }
+        // TODO Check we have a sink
+        final boolean out = q.offer(in);
+        return out;
+    }
+
+    /**
+     * Gets the timeout on fetch operations of the instance.
+     * 
+     * @return the fetch timeout.
+     */
+    public long getFetchTimeout() {
+        return fetchTimeout.get();
+    }
+
+    /**
+     * Sets the fetch timeout of the instance. Positive values represent
+     * milliseconds to wait, and 0 means to not wait at all.
+     * 
+     * @param fetchTimeout
+     *            the fetch timeout to set
+     * @throws IllegalArgumentException
+     *             If the argument is < 0.
+     */
+    public void setFetchTimeout(final long newFetchTimeout) {
+        if (newFetchTimeout < 0) {
+            throw new IllegalArgumentException("Fetch timeout must be nonnegative");
+        }
+        fetchTimeout.set(newFetchTimeout);
+    }
+
+    /**
+     * Removes an element from the link, if any is available. This method may
+     * block for up to {@link #getFetchTimeout()} milliseconds for a value to
+     * become available.
+     * 
+     * @return The removed element, or <code>null</code> if the link is empty.
+     */
+    public T get() {
+        // TODO If we don't have a source, don't bother waiting on the timeout
+        try {
+            return q.poll(max(1, getFetchTimeout()), TimeUnit.MILLISECONDS);
+        } catch (InterruptedException ie) {
+            return null;
+        }
+    }
 }
--- trunk/src/jconch/pipeline/Producer.java
+++ trunk/src/jconch/pipeline/Producer.java
@@ -15,120 +15,119 @@ import org.apache.commons.lang.NullArgumentException;
  */
 public abstract class Producer<OUT_T> extends PipelineStage {
 
-	/**
-	 * The link we drop into.
-	 */
-	protected final PipeLink<OUT_T> link;
+    /**
+     * The link we drop into.
+     */
+    protected final PipeLink<OUT_T> link;
 
-	/**
-	 * If we've created <code>null</code> before.
-	 */
-	private final AtomicBoolean createdNull = new AtomicBoolean(false);
+    /**
+     * If we've created <code>null</code> before.
+     */
+    private final AtomicBoolean createdNull = new AtomicBoolean(false);
 
-	/**
-	 * If we have failed to do an add before.
-	 */
-	private final AtomicBoolean failedAdd = new AtomicBoolean(false);
+    /**
+     * If we have failed to do an add before.
+     */
+    private final AtomicBoolean failedAdd = new AtomicBoolean(false);
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param threading
-	 *            The threading model.
-	 * @param link
-	 *            The link we produce things into.
-	 * @throws NullArgumentException
-	 *             If either argument is <code>null</code>.
-	 */
-	protected Producer(final ThreadingModel threading,
-			final PipeLink<OUT_T> link) {
-		super(threading);
-		if (link == null) {
-			throw new NullArgumentException("link");
-		}
-		this.link = link;
-	}
+    /**
+     * Constructor.
+     * 
+     * @param threading
+     *            The threading model.
+     * @param link
+     *            The link we produce things into.
+     * @throws NullArgumentException
+     *             If either argument is <code>null</code>.
+     */
+    protected Producer(final ThreadingModel threading, final PipeLink<OUT_T> link) {
+        super(threading);
+        if (link == null) {
+            throw new NullArgumentException("link");
+        }
+        this.link = link;
+        this.link.registerSource(this);
+    }
 
-	/**
-	 * Provides the pipeline link out.
-	 * 
-	 * @return The link that the producer feeds into; never <code>null</code>.
-	 */
-	public PipeLink<OUT_T> getLinkOut() {
-		return link;
-	}
+    /**
+     * Provides the pipeline link out.
+     * 
+     * @return The link that the producer feeds into; never <code>null</code>.
+     */
+    public PipeLink<OUT_T> getLinkOut() {
+        return link;
+    }
 
-	/**
-	 * Method that must be implemented to queue the producer.
-	 * 
-	 * @return The next item for the producer, or <code>null</code> if there
-	 *         are no more elements.
-	 */
-	public abstract OUT_T produceItem();
+    /**
+     * Method that must be implemented to queue the producer.
+     * 
+     * @return The next item for the producer, or <code>null</code> if there
+     *         are no more elements.
+     */
+    public abstract OUT_T produceItem();
 
-	/**
-	 * If more elements will be produced. If the current state is indeterminant,
-	 * this must block until it has an answer.
-	 * 
-	 * @return If there are more elements.
-	 */
-	public abstract boolean isExhausted();
+    /**
+     * If more elements will be produced. If the current state is indeterminant,
+     * this must block until it has an answer.
+     * 
+     * @return If there are more elements.
+     */
+    public abstract boolean isExhausted();
 
-	/**
-	 * In addition to the basic checks, checks for error conditions.
-	 * 
-	 * {@inheritDoc}
-	 */
-	@Override
-	public boolean isFinished() {
-		return super.isFinished() || createdNull.get() || failedAdd.get();
-	}
+    /**
+     * In addition to the basic checks, checks for error conditions.
+     * 
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isFinished() {
+        return super.isFinished() || createdNull.get() || failedAdd.get();
+    }
 
-	/**
-	 * Checks state, generates an element, and then puts it into the queue.
-	 */
-	@Override
-	final void execute() {
-		// Make sure we're not already exhausted
-		final IllegalStateException ise;
-		if (createdNull.get()) {
-			ise = new IllegalStateException("Previously created null element");
-		} else if (failedAdd.get()) {
-			ise = new IllegalStateException("Previously failed an add");
-		} else if (isExhausted()) {
-			ise = new IllegalStateException("Pipeline is exhausted");
-		} else if (isFinished()) {
-			ise = new IllegalStateException("Indeterminant reason");
-		} else {
-			ise = null;
-		}
-		if (ise != null) {
-			logMessage("Called execute at wrong time", ise);
-			return;
-		}
+    /**
+     * Checks state, generates an element, and then puts it into the queue.
+     */
+    @Override
+    final void execute() {
+        // Make sure we're not already exhausted
+        final IllegalStateException ise;
+        if (createdNull.get()) {
+            ise = new IllegalStateException("Previously created null element");
+        } else if (failedAdd.get()) {
+            ise = new IllegalStateException("Previously failed an add");
+        } else if (isExhausted()) {
+            ise = new IllegalStateException("Pipeline is exhausted");
+        } else if (isFinished()) {
+            ise = new IllegalStateException("Indeterminant reason");
+        } else {
+            ise = null;
+        }
+        if (ise != null) {
+            logMessage("Called execute at wrong time", ise);
+            return;
+        }
 
-		// Produce an element
-		final OUT_T out;
-		try {
-			out = produceItem();
-		} catch (final Exception e) {
-			logMessage("Exception when attempting to produce an item", e);
-			return;
-		}
+        // Produce an element
+        final OUT_T out;
+        try {
+            out = produceItem();
+        } catch (final Exception e) {
+            logMessage("Exception when attempting to produce an item", e);
+            return;
+        }
 
-		// Check to see we've got something
-		if (out == null) {
-			createdNull.set(true);
-			logMessage("Null element retrieved", new NoSuchElementException(
-					"Cannot generate more elements"));
-			return;
-		}
+        // Check to see we've got something
+        if (out == null) {
+            createdNull.set(true);
+            logMessage("Null element retrieved", new NoSuchElementException("Cannot generate more elements"));
+            return;
+        }
 
-		try {
-			failedAdd.set(!this.link.add(out));
-		} catch (Exception e) {
-			failedAdd.set(true);
-			logMessage("Exception when attempting to write item", e);
-		}
-	}
+        try {
+            failedAdd.set(!this.link.add(out));
+        } catch (Exception e) {
+            failedAdd.set(true);
+            logMessage("Exception when attempting to write item", e);
+        }
+    }
 }
