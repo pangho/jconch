b3501107f6fd8a024445065e007d1c13f6db6dcf
fvmc.conch <fvmc.conch@2cae41e6-9f2a-0410-8235-f93780899747>
2007-05-14 12:49:37 +0000

    Updated ThreadingModels to use #isFinished instead of #isExhausted, and updated PipeLink to track weak references to their sources and sinks.

--- src/jconch/pipeline/SingleThreadThreadingModel.java
+++ src/jconch/pipeline/SingleThreadThreadingModel.java
@@ -9,24 +9,28 @@ import org.apache.commons.lang.NullArgumentException;
  */
 public class SingleThreadThreadingModel implements ThreadingModel {
 
-	/**
-	 * Spawns a thread, and it keeps executing everything.
-	 * 
-	 * @throws NullArgumentException
-	 *             If the argument is <code>null</code>
-	 */
-	public void execute(final PipelineStage toRun) {
-		if (toRun == null) {
-			throw new NullArgumentException("toRun");
-		}
-		new Thread() {
-			@Override
-			public void run() {
-				while (!toRun.isExhausted()) {
-					toRun.execute();
-				}
-			}
-		}.start();
-	}
+    /**
+     * Spawns a thread, and it keeps executing everything.
+     * 
+     * @throws NullArgumentException
+     *             If the argument is <code>null</code>
+     */
+    public void execute(final PipelineStage toRun) {
+        if (toRun == null) {
+            throw new NullArgumentException("toRun");
+        }
+        new Thread() {
+            @Override
+            public void run() {
+                while (!toRun.isFinished()) {
+                    try {
+                        toRun.execute();
+                    } catch (Exception e) {
+                        toRun.logMessage("Unhandled error during execute", e);
+                    }
+                }
+            }
+        }.start();
+    }
 
 }
--- src/jconch/pipeline/SpawningThreadingModel.java
+++ src/jconch/pipeline/SpawningThreadingModel.java
@@ -1,6 +1,7 @@
 package jconch.pipeline;
 
 import org.apache.commons.lang.NullArgumentException;
+import org.apache.commons.lang.time.DateUtils;
 
 /**
  * Spawns a new thread for each call to execute.
@@ -10,49 +11,53 @@ import org.apache.commons.lang.NullArgumentException;
  */
 public class SpawningThreadingModel implements ThreadingModel {
 
-	private final long waitTime;
+    private final long waitTime;
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param waitBetweenSpawns
-	 *            The time to wait between spawns.
-	 * @throws IllegalArgumentException
-	 *             If the argument <= 0
-	 */
-	public SpawningThreadingModel(final long waitBetweenSpawns) {
-		if (waitBetweenSpawns <= 0) {
-			throw new IllegalArgumentException("Argument cannot be 0 or less");
-		}
-		this.waitTime = waitBetweenSpawns;
-	}
+    /**
+     * Constructor that uses a default wait time between spawns.
+     */
+    public SpawningThreadingModel() {
+        this.waitTime = 1 * DateUtils.MILLIS_PER_SECOND;
+    }
 
-	/**
-	 * Spawns a thread that calls {@link PipelineStage#execute()}.
-	 * 
-	 * @throws NullArgumentException
-	 *             If the argument is <code>null</code>
-	 */
-	public void execute(final PipelineStage toRun) {
-		if (toRun == null) {
-			throw new NullArgumentException("toRun");
-		}
-		while (!toRun.isExhausted()) {
-			final Thread t = new Thread() {
-				@Override
-				public void run() {
-					if (!toRun.isExhausted()) {
-						toRun.execute();
-					}
-				}
-			};
-			t.start();
-			try {
-				t.join(waitTime);
-			} catch (InterruptedException e) {
-				Thread.yield();
-			}
-		}
-	}
+    /**
+     * Constructor.
+     * 
+     * @param waitBetweenSpawns
+     *            The time to wait between spawns.
+     * @throws IllegalArgumentException
+     *             If the argument <= 0
+     */
+    public SpawningThreadingModel(final long waitBetweenSpawns) {
+        if (waitBetweenSpawns <= 0) {
+            throw new IllegalArgumentException("Argument cannot be 0 or less");
+        }
+        this.waitTime = waitBetweenSpawns;
+    }
 
+    /**
+     * Spawns a thread that calls {@link PipelineStage#execute()}.
+     * 
+     * @throws NullArgumentException
+     *             If the argument is <code>null</code>
+     */
+    public void execute(final PipelineStage toRun) {
+        if (toRun == null) {
+            throw new NullArgumentException("toRun");
+        }
+        while (!toRun.isFinished()) {
+            final Thread t = new Thread() {
+                @Override
+                public void run() {
+                    toRun.execute();
+                }
+            };
+            t.start();
+            try {
+                t.join(waitTime);
+            } catch (InterruptedException e) {
+                Thread.yield();
+            }
+        }
+    }
 }
