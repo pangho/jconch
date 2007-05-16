e276e8f2e2a2f3d5416868e544eae048c2c450ac
fvmc.conch <fvmc.conch@2cae41e6-9f2a-0410-8235-f93780899747>
2007-05-16 23:46:25 +0000

    Implemented some tests, and a bit of nudging because of them.

--- test/jconch/pipeline/PipeLinkTest.java
+++ test/jconch/pipeline/PipeLinkTest.java
@@ -1,12 +1,12 @@
 package jconch.pipeline;
 
-import static org.junit.Assert.assertTrue;
-import static test.utils.MemoryTestUtils.forceGC;
+import static org.junit.Assert.*;
 
-import java.util.concurrent.SynchronousQueue;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
 
 import org.apache.commons.lang.NullArgumentException;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -21,7 +21,7 @@ public class PipeLinkTest {
      * @return A newly created PipeLink to test.
      */
     protected PipeLink<Object> createFixture() {
-        return new PipeLink<Object>(new SynchronousQueue<Object>());
+        return new PipeLink<Object>(new ArrayBlockingQueue<Object>(1));
     }
 
     @Test(expected = NullArgumentException.class)
@@ -32,6 +32,54 @@ public class PipeLinkTest {
     @Test(expected = NullArgumentException.class)
     public void registerSourceExplodesOnNull() {
         createFixture().registerSource(null);
+    }
+
+    @Test
+    public void runAnElementThrough() {
+        final Object testMonkey = new Object();
+        final PipeLink<Object> fixture = createFixture();
+        assertTrue("Add failed", fixture.add(testMonkey));
+        final Object outMonkey = fixture.get();
+        assertNotNull("Retrieved null", outMonkey);
+        assertSame("Retrieved something different", testMonkey, outMonkey);
+    }
+
+    @Test
+    public void breakLinkBreaksAdd() {
+        final Object testMonkey = new Object();
+        final PipeLink<Object> fixture = createFixture();
+        fixture.breakLink();
+        assertFalse("Add should have failed", fixture.add(testMonkey));
+    }
+
+    @Test
+    public void breakLinkBreaksGet() {
+        final Object testMonkey = new Object();
+        final PipeLink<Object> fixture = createFixture();
+        assertTrue("Add failed", fixture.add(testMonkey));
+        fixture.breakLink();
+        final Object outMonkey = fixture.get();
+        assertNull("Retrieved an object -- should be null", outMonkey);
+    }
+
+    @Test
+    public void setAddTimeoutDoesNotExplodeOnZero() {
+        createFixture().setAddTimeout(0);
+    }
+
+    @Test
+    public void setFetchTimeoutDoesNotExplodeOnZero() {
+        createFixture().setFetchTimeout(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setAddTimeoutExplodesOnNegative() {
+        createFixture().setAddTimeout(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setFetchTimeoutExplodesOnNegative() {
+        createFixture().setFetchTimeout(-1);
     }
 
 }
