4ab852fdb1cc676a42ecc96fb1dc605e55107f5e
fvmc.conch <fvmc.conch@2cae41e6-9f2a-0410-8235-f93780899747>
2007-05-16 13:24:29 +0000

    Added a bounded version of the PipeLink.  At this point, the users should be happy!

---
+++ src/jconch/pipeline/BoundedPipeLink.java
@@ -0,0 +1,23 @@
+package jconch.pipeline;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * A pipe link with a specified upper bound on the queue length.
+ */
+public class BoundedPipeLink<T> extends PipeLink<T> {
+
+    /**
+     * Constructor.
+     * 
+     * @param maxElements
+     *            The maximum number of elements.
+     * @throws IllegalArgumentException
+     *             If the argument is not greater than zero.
+     */
+    public BoundedPipeLink(final int maxElements) {
+        super(new LinkedBlockingQueue<T>(maxElements));
+    }
+
+}
