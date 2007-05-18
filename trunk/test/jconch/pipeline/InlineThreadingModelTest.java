20066ab8a2eb3c5e36e8b36006f242c5ac83f304
fvmc.conch <fvmc.conch@2cae41e6-9f2a-0410-8235-f93780899747>
2007-05-18 13:22:31 +0000

    Started with the ExecutorThreadingModel tests...and found something to debug.

--- trunk/test/jconch/pipeline/InlineThreadingModelTest.java
+++ trunk/test/jconch/pipeline/InlineThreadingModelTest.java
@@ -6,17 +6,19 @@ import static org.junit.Assert.*;
 
 public class InlineThreadingModelTest {
 
-	@Test(expected = NullArgumentException.class)
-	public void executeExplodesOnNull() {
-		new InlineThreadingModel().execute(null);
-	}
+    @Test(expected = NullArgumentException.class)
+    public void executeExplodesOnNull() {
+        new InlineThreadingModel().execute(null);
+    }
 
-	@Test
-	public void executeCalledOnSameThread() {
-		final InlineThreadingModel fixture = new InlineThreadingModel();
-		final ThreadCapturingPipelineStage stage = new ThreadCapturingPipelineStage(
-				fixture);
-		stage.execute();
-		assertSame("Thread is different", Thread.currentThread(), stage.executeThread);
-	}
+    @Test
+    public void executeCalledOnSameThread() {
+        final InlineThreadingModel fixture = new InlineThreadingModel();
+        final ThreadCapturingPipelineStage stage = new ThreadCapturingPipelineStage(fixture, 10);
+        stage.start();
+        assertTrue("Did not finish inline", stage.isFinished());
+        for (final Thread t : stage.executeThreads) {
+            assertSame("Thread is different", Thread.currentThread(), t);
+        }
+    }
 }
--- trunk/test/jconch/pipeline/ThreadCapturingPipelineStage.java
+++ trunk/test/jconch/pipeline/ThreadCapturingPipelineStage.java
@@ -1,5 +1,11 @@
 package jconch.pipeline;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.collections.ListUtils;
+import org.apache.commons.lang.time.DateUtils;
+
 /**
  * A pipeline stage that simply captures the thread that executed it. Useful in
  * unit tests to catch threading models.
@@ -8,25 +14,34 @@ package jconch.pipeline;
  */
 class ThreadCapturingPipelineStage extends PipelineStage {
 
-	public Thread executeThread = null;
+    public final List<Thread> executeThreads;
 
-	public ThreadCapturingPipelineStage(final ThreadingModel threading) {
-		super(threading);
-	}
+    public final int rounds;
 
-	@Override
-	void execute() {
-		executeThread = Thread.currentThread();
-	}
+    public ThreadCapturingPipelineStage(final ThreadingModel threading, final int rounds) {
+        super(threading);
+        this.executeThreads = ListUtils.typedList(new ArrayList(rounds + 1), Thread.class);
+        this.rounds = rounds;
+    }
 
-	@Override
-	protected void logMessage(String msg, Exception e) {
-		// Does nothing
-	}
+    @Override
+    void execute() {
+        executeThreads.add(Thread.currentThread());
+        try {
+            Thread.sleep(DateUtils.MILLIS_PER_SECOND / 10);
+        } catch (InterruptedException e) {
+            Thread.yield();
+        }
+    }
 
-	@Override
-	public boolean isFinished() {
-		return executeThread != null;
-	}
+    @Override
+    protected void logMessage(String msg, Exception e) {
+        // Does nothing
+    }
+
+    @Override
+    public boolean isFinished() {
+        return executeThreads.size() == rounds;
+    }
 
 }
