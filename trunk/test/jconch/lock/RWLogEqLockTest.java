1513c2bb849a90a001bff4a7863b558a765b677e
fvmc.conch <fvmc.conch@2cae41e6-9f2a-0410-8235-f93780899747>
2007-12-29 06:52:01 +0000

    Updated/fixed up the LogEqLock tests.

--- trunk/test/jconch/lock/RWLogEqLockTest.java
+++ trunk/test/jconch/lock/RWLogEqLockTest.java
@@ -4,6 +4,7 @@ import static org.testng.AssertJUnit.*;
 
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.ReadWriteLock;
 
 import org.testng.annotations.Test;
 
@@ -12,54 +13,51 @@ import org.testng.annotations.Test;
  * 
  * @author Robert Fischer
  */
-public class RWLogEqLockTest extends AbstractLogEqLockTest<RWLogEqLock> {
+public class RWLogEqLockTest extends AbstractLogEqLockTest<ReadWriteLock, RWLogEqLock<Long>> {
 
-	@Override
-	protected RWLogEqLock createTestInstance() {
-		return new RWLogEqLock();
-	}
+    @Override
+    protected RWLogEqLock<Long> createTestInstance() {
+        return new RWLogEqLock<Long>();
+    }
 
-	@Test
-	public void testGetGlobalInstance() {
-		final int iterations = 10000;
-		final int maxWaitPerThread = 100;
-		final SyncLogEqLock global = SyncLogEqLock.getGlobalInstance();
-		assertNotNull(global);
+    @Test
+    public void testGetGlobalInstance() {
+        final int iterations = 1000;
+        final int maxWaitPerThread = 20;
+        final RWLogEqLock<Object> global = RWLogEqLock.getGlobalInstance();
+        assertNotNull(global);
 
-		// Now check to see if other threads ever see something different
-		final AtomicBoolean sawSomethingDifferent = new AtomicBoolean(false);
-		for (int i = 0; i < iterations; i++) {
-			final int waitBeforeRetrieve = new Random()
-					.nextInt(maxWaitPerThread) + 1;
-			assertFalse(0 == waitBeforeRetrieve);
-			final Runnable createAndCheck = new Runnable() {
-				public void run() {
-					// Wait some arbitrary time
-					try {
-						Thread.sleep(waitBeforeRetrieve);
-					} catch (InterruptedException ie) {
-						Thread.yield();
-					}
+        // Now check to see if other threads ever see something different
+        final AtomicBoolean sawSomethingDifferent = new AtomicBoolean(false);
+        for (int i = 0; i < iterations; i++) {
+            final int waitBeforeRetrieve = new Random().nextInt(maxWaitPerThread) + 1;
+            assertFalse(0 == waitBeforeRetrieve);
+            final Runnable createAndCheck = new Runnable() {
+                public void run() {
+                    // Wait some arbitrary time
+                    try {
+                        Thread.sleep(waitBeforeRetrieve);
+                    } catch (InterruptedException ie) {
+                        Thread.yield();
+                    }
 
-					// Now check to see if we get the same global
-					final SyncLogEqLock myGlobal = SyncLogEqLock
-							.getGlobalInstance();
-					if (myGlobal != global) {
-						sawSomethingDifferent.set(true);
-					}
-				}
-			};
-			new Thread(createAndCheck).start();
-			assertFalse("Saw something different", sawSomethingDifferent.get());
-		}
+                    // Now check to see if we get the same global
+                    final RWLogEqLock<Object> myGlobal = RWLogEqLock.getGlobalInstance();
+                    if (myGlobal != global) {
+                        sawSomethingDifferent.set(true);
+                    }
+                }
+            };
+            new Thread(createAndCheck).start();
+            assertFalse("Saw something different", sawSomethingDifferent.get());
+        }
 
-		// Give things a chance to finish up.
-		try {
-			Thread.sleep(10 * maxWaitPerThread);
-		} catch (final InterruptedException ie) {
-			Thread.yield();
-		}
-		assertFalse("Saw something different", sawSomethingDifferent.get());
-	}
-
+        // Give things a chance to finish up.
+        try {
+            Thread.sleep(10 * maxWaitPerThread);
+        } catch (final InterruptedException ie) {
+            Thread.yield();
+        }
+        assertFalse("Saw something different", sawSomethingDifferent.get());
+    }
 }
--- trunk/test/jconch/lock/SyncLogEqLockTest.java
+++ trunk/test/jconch/lock/SyncLogEqLockTest.java
@@ -7,54 +7,52 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.testng.annotations.Test;
 
-public class SyncLogEqLockTest extends AbstractLogEqLockTest<SyncLogEqLock> {
+public class SyncLogEqLockTest extends AbstractLogEqLockTest<Object, SyncLogEqLock<Long>> {
 
-	@Override
-	protected SyncLogEqLock createTestInstance() {
-		return new SyncLogEqLock();
-	}
+    @Override
+    protected SyncLogEqLock<Long> createTestInstance() {
+        return new SyncLogEqLock<Long>();
+    }
 
-	@Test
-	public void testGetGlobalInstance() {
-		final int iterations = 10000;
-		final int maxWaitPerThread = 100;
-		final SyncLogEqLock global = SyncLogEqLock.getGlobalInstance();
-		assertNotNull(global);
+    @Test
+    public void testGetGlobalInstance() {
+        final int iterations = 1000;
+        final int maxWaitPerThread = 20;
+        final SyncLogEqLock<Object> global = SyncLogEqLock.getGlobalInstance();
+        assertNotNull(global);
 
-		// Now check to see if other threads ever see something different
-		final AtomicBoolean sawSomethingDifferent = new AtomicBoolean(false);
-		for (int i = 0; i < iterations; i++) {
-			final int waitBeforeRetrieve = new Random()
-					.nextInt(maxWaitPerThread) + 1;
-			assertFalse(0 == waitBeforeRetrieve);
-			final Runnable createAndCheck = new Runnable() {
-				public void run() {
-					// Wait some arbitrary time
-					try {
-						Thread.sleep(waitBeforeRetrieve);
-					} catch (InterruptedException ie) {
-						Thread.yield();
-					}
+        // Now check to see if other threads ever see something different
+        final AtomicBoolean sawSomethingDifferent = new AtomicBoolean(false);
+        for (int i = 0; i < iterations; i++) {
+            final int waitBeforeRetrieve = new Random().nextInt(maxWaitPerThread) + 1;
+            assertFalse(0 == waitBeforeRetrieve);
+            final Runnable createAndCheck = new Runnable() {
+                public void run() {
+                    // Wait some arbitrary time
+                    try {
+                        Thread.sleep(waitBeforeRetrieve);
+                    } catch (InterruptedException ie) {
+                        Thread.yield();
+                    }
 
-					// Now check to see if we get the same global
-					final SyncLogEqLock myGlobal = SyncLogEqLock
-							.getGlobalInstance();
-					if (myGlobal != global) {
-						sawSomethingDifferent.set(true);
-					}
-				}
-			};
-			new Thread(createAndCheck).start();
-			assertFalse("Saw something different", sawSomethingDifferent.get());
-		}
+                    // Now check to see if we get the same global
+                    final SyncLogEqLock<Object> myGlobal = SyncLogEqLock.getGlobalInstance();
+                    if (myGlobal != global) {
+                        sawSomethingDifferent.set(true);
+                    }
+                }
+            };
+            new Thread(createAndCheck).start();
+            assertFalse("Saw something different", sawSomethingDifferent.get());
+        }
 
-		// Give things a chance to finish up.
-		try {
-			Thread.sleep(10 * maxWaitPerThread);
-		} catch (final InterruptedException ie) {
-			Thread.yield();
-		}
-		assertFalse("Saw something different", sawSomethingDifferent.get());
-	}
+        // Give things a chance to finish up.
+        try {
+            Thread.sleep(10 * maxWaitPerThread);
+        } catch (final InterruptedException ie) {
+            Thread.yield();
+        }
+        assertFalse("Saw something different", sawSomethingDifferent.get());
+    }
 
 }
