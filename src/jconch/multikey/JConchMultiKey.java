141f61e860d0adfdb0fa97dc00b31d6dde823d7b
robert.fischer <robert.fischer@SmokejumperIT.com>
2008-03-14 22:57:28 +0000

    Now caches the hash code on the multikeys.

--- src/jconch/multikey/JConchMultiKey.java
+++ src/jconch/multikey/JConchMultiKey.java
@@ -5,24 +5,51 @@ import org.apache.commons.lang.math.RandomUtils;
 
 import java.util.List;
 
+/**
+ * A fast, typed, non-modifiable, thread-safe implementation of having multiple keys for a map.
+ */
 public abstract class JConchMultiKey {
 
-    private static final int BASE_HASH_CODE = RandomUtils.nextInt();
-
+    /**
+     * The number of elements in the multikey.
+     *
+     * @return The cardinality of this implementation.
+     */
     public abstract int getKeyCount();
 
+    /**
+     * The keys that make up the multikey.
+     *
+     * @return An unmodifiable list of keys that make up the map.
+     */
     public abstract List<Object> getKeys();
+
+    /**
+     * An arbitrary value from which to start our hash code algorithm.
+     */
+    private static final int BASE_HASH_CODE = RandomUtils.nextInt();
+
+    /**
+     * The cached hash code for this map.  Cached so that we get thread safety and speed, and because it's
+     * almost inevitably going to be used in any case.
+     */
+    private final int hashCode;
+
+    public JConchMultiKey() {
+        int myHashCode = BASE_HASH_CODE;
+        for (final Object key : getKeys()) {
+            if (key == null) {
+                myHashCode = ~myHashCode + 1;
+            } else {
+                myHashCode = myHashCode ^ key.hashCode();
+            }
+        }
+        hashCode = myHashCode;
+    }
+
 
     @Override
     public int hashCode() {
-        int hashCode = BASE_HASH_CODE;
-        for (final Object key : getKeys()) {
-            if (key == null) {
-                hashCode = ~hashCode + 1;
-            } else {
-                hashCode = hashCode ^ key.hashCode();
-            }
-        }
         return hashCode;
     }
 
